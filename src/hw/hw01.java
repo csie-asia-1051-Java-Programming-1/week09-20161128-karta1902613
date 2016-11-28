c2aaf3c907b14c2751b345f7a3eb10a07a4d5f23
karta1902613 <105021043@live.asia.edu.tw>
2016-11-28 17:59:52 +0800

    hw 12 1128 1.0
    hw 12 1128 1.0

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -3,13 +3,37 @@ package hw;
  * Topic: 讓使用者輸入一正整數，利用函式呼叫找出一連續整數數列讓該數列的和為此一正整數，有些正整數並不能找出連續整數，若找不到時，請印出「No」。
  例如: 15 =1+2+3+4+5 =4+5+6=7+8
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021043 劉凱文
  */
+import java.util.Scanner;
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		fun(n);
 	}
-
+	public static void fun(int n){
+		int sum=0;
+		int i=0;
+		
+		while(sum<=n-1){
+			i++;
+			sum+=i;		
+			
+		}
+		if(sum==n){
+			
+		System.out.print(sum);
+		}
+		else{
+			System.out.print("no");
+		}
+					
+			
+		
+		
+	}
+	
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -4,13 +4,31 @@ package hw;
 數列一:1 2 4 7 11 16 22 29…
 數列二:1 2 2 3 3 3 4 4 4 4 5 5 5 5 5...
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021043 劉凱文
  */
+import java.util.Scanner;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int x=1;
+		int i=0;	
+		System.out.print("數列一："+" ");
+		while(n>=x){
+			x+=i;
+			i++;
+			System.out.print(x+" ");			
+		}
+		System.out.println("");
+		int y=1;
+		int j=0;
+		System.out.print("數列二："+" ");
+	    while(y<=n){    	
+	    	for(j=0;j<y;j++){
+	    	System.out.print(y+" ");}
+	    	y++;
+	    }
 	}
-
 }
